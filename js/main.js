f701a021c5b6d506807ea1e1490e50aac4d59e0d
seif allah ben ahmed <benahmed.seifallah@gmail.com>
2021-07-21 18:28:09 +0200

    Screenshots Section

--- js/main.js
+++ js/main.js
@@ -39,4 +39,22 @@ $(document).ready(function () {
             },
         },
     });
+    /*-------------------------App Sreenshot Carousel-------------------------- */
+    $(".screenshots-carousel").owlCarousel({
+        loop: true,
+        margin: 0,
+        autoplay: true,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1,
+            },
+            600: {
+                items: 3,
+            },
+            1000: {
+                items: 4,
+            },
+        },
+    });
 });
