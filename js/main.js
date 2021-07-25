b531ab2d2b0d939c768afe8cead3957292609ad6
seif allah ben ahmed <benahmed.seifallah@gmail.com>
2021-07-25 21:14:34 +0200

    Toggle Theme Start - Light & Dark mode

--- js/main.js
+++ js/main.js
@@ -100,4 +100,34 @@ $(document).ready(function () {
     $(".nav-link").on("click", function () {
         $(".navbar-collapse").collapse("hide");
     });
+    /*-------------------------Toggle Theme - Light & Dark-------------------------- */
+    function toggleTheme() {
+        if (localStorage.getItem("bootstrap-theme") !== null) {
+            if (localStorage.getItem("bootstrap-theme") === "dark") {
+                $("body").addClass("dark");
+            } else {
+                $("body").removeClass("dark");
+            }
+        }
+        updateIcon();
+    }
+    toggleTheme();
+    $(".toggle-theme").on("click", function () {
+        $("body").toggleClass("dark");
+        if ($("body").hasClass("dark")) {
+            localStorage.setItem("bootstrap-theme", "dark");
+        } else {
+            localStorage.setItem("bootstrap-theme", "light");
+        }
+        updateIcon();
+    });
+    function updateIcon() {
+        if ($("body").hasClass("dark")) {
+            $(".toggle-theme i").removeClass("fa-moon");
+            $(".toggle-theme i").addClass("fa-sun");
+        } else {
+            $(".toggle-theme i").removeClass("fa-sun");
+            $(".toggle-theme i").addClass("fa-moon");
+        }
+    }
 });
